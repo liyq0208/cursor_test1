85734e9b7fbfcd445d0849f41060df2aa42b1935
liyq <673005973@qq.com>
2026-03-24 00:30:50 +0800

    2切片写法

--- day2/2.py
+++ day2/2.py
@@ -8,4 +8,12 @@ print('接口类型: '+int_type)
 print('接口编号: '+int_num)
 
 
+# 修改为切片写法
+print(interface[:-5])
+print(interface[-5:])
+
+print('接口类型: '+interface[:-5])
+print('接口编号: '+interface[-5:])
+
+
 
